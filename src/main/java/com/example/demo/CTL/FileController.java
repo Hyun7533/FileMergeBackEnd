9866f7db7a886502d0fc0ccbbad09e7f20c092d1
KimTaeHyun <xogus7533@naver.com>
2022-12-03 16:11:24 +0900

    2022-12-03 필요없는 코드 삭제

--- src/main/java/com/example/demo/CTL/FileController.java
+++ src/main/java/com/example/demo/CTL/FileController.java
@@ -1,19 +1,18 @@
 package com.example.demo.CTL;
 
-import ch.qos.logback.core.pattern.color.BoldYellowCompositeConverter;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfImportedPage;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfWriter;
 import jakarta.annotation.Nullable;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
-import java.sql.Blob;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -22,47 +21,76 @@ public class FileController {
 
     @ResponseBody
     @PostMapping(value = "/mergeFile")
-    public void FileMerge(@RequestParam(value = "file") List<byte[]> file) {
+    public OutputStream FileMerge(@RequestBody @Nullable List<byte[]> file) throws IOException, DocumentException {
+        List<InputStream> list = new ArrayList<>();
+//            InputStream inputStream1 = new ByteArrayInputStream(file.get(0));
 
-        try {
+        InputStream inputStream1 = new FileInputStream(new File("C:\\test1.pdf"));
+        list.add(inputStream1);
+//            InputStream inputStream2 = new ByteArrayInputStream(file.get(1));
+        InputStream inputStream2 = new FileInputStream(new File("C:\\test2.pdf"));
+        list.add(inputStream2);
 
+        OutputStream outputStream = new FileOutputStream(new File("testMerge.pdf")); // 합칠 문서가 생성된다 열면 에러남
 
-            System.out.println("file.get(0) = " + file.get(0));
-            System.out.println("file.get(0) = " + file.get(1));
+        Document document = new Document(); // 문서 만들고
+        PdfWriter pdfWriter = PdfWriter.getInstance(document, outputStream);
+        document.open();
+        PdfContentByte pdfContentByte = pdfWriter.getDirectContent();
 
-            List<InputStream> list = new ArrayList<>();
-            InputStream inputStream1 = new ByteArrayInputStream(file.get(0));
-            list.add(inputStream1);
-
-            InputStream inputStream2 = new ByteArrayInputStream(file.get(1));
-            list.add(inputStream2);
-
-            OutputStream outputStream = new FileOutputStream(new File("mergedPdf.pdf"));
-
-            Document document = new Document(); // 문서 만들고
-            PdfWriter pdfWriter = PdfWriter.getInstance(document, outputStream);
-            document.open();
-            PdfContentByte pdfContentByte = pdfWriter.getDirectContent();
-
-            for (InputStream inputStream : list)
-            {
-                PdfReader pdfReader = new PdfReader(inputStream);
-                for (int i = 1; i <= pdfReader.getNumberOfPages(); i++)
-                {
-                    document.newPage();
-                    PdfImportedPage page = pdfWriter.getImportedPage(pdfReader, i);
-                    pdfContentByte.addTemplate(page, 0, 0);
-                }
+        for (InputStream inputStream : list) {
+            PdfReader pdfReader = new PdfReader(inputStream);
+            for (int i = 1; i <= pdfReader.getNumberOfPages(); i++) {
+                document.newPage();
+                PdfImportedPage page = pdfWriter.getImportedPage(pdfReader, i);
+                pdfContentByte.addTemplate(page, 0, 0);
             }
-
-            outputStream.flush();
-            document.close();
-            outputStream.close();
-
-
-        } catch (Exception e) {
-            System.out.println("e = " + e);
         }
+
+        outputStream.flush();
+        document.close();
+        outputStream.close();
+
+
+        return outputStream;
+
+    }
+
+    @ResponseBody
+    @PostMapping("/file")
+    public void File(@RequestBody HashMap<String, Object> file) {
+        System.out.println("file = " + file.get("file"));
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
